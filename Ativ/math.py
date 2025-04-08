1b116fa97bf4d9d1a51706d1863e3430772e97cf
Gustavo-Hyppolito <gustavo.h.rodrigues10@aluno.senai.br>
2025-04-08 09:08:26 -0300

    Criei Math.py
    Para armazenar o código que posteriormente vai ser analisado

---
+++ Ativ/math.py
@@ -0,0 +1,22 @@
+
+def conta(num1, num2):
+    resultado=0
+    if operação =="+":
+       resultado= num1+num2
+     
+       print(resultado)
+
+    elif operação== "-":
+        
+         resultado=num1-num2
+         print(resultado)
+    elif operação== "*":
+          resultado=num1*num2
+          print(resultado)
+    elif operação=='/':
+        resultado= num1/num2
+        print (resultado)
+
+conta (num1, num2)
+
+
